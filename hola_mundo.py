387a35eac6cfdb214f5fb8d8cd1450f45d52049d
Miguel Loaiza <miguel.mejial@upb.edu.co>
2026-01-23 15:02:07 -0500

    se puso el codigo de python

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,2 +1,19 @@
 # mi_primer_script.py
 print("¡Hola, GitHub!")
+
+primos = []
+
+for numero in range(1, 1001):
+    if numero > 1:
+        es_primo = True
+
+        for i in range(2, numero):
+            if numero % i == 0:
+                es_primo = False
+                break
+
+        if es_primo:
+            primos.append(numero)
+
+print("Números primos entre 1 y 1000:")
+print(primos)
